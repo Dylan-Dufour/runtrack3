87536a2b6be3d07c3ba11b38ac27d879cc31f5cd
Dylan-Dufour <dylan.dufour@laplateforme.io>
2025-10-28 14:16:35 +0100

    Modifier l’affichage de sorte à ce que le message ne soit plus écrit dans une popup mais dans la console web.

---
+++ job03/index1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Exemple</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <h1>Bienvenue sur mon site</h1>
+</body>
+</html>
