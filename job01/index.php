34c94e874d855cb254ccea2a00cea3bb4e6494bd
Dylan-Dufour <dylan.dufour@laplateforme.io>
2025-10-30 09:27:11 +0100

    La fonction de récupération et d’affichage doit se nommer “citation()”.

---
+++ job01/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Job 01</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <article id="citation">
+        La vie a beaucoup plus d'imagination que nous
+    </article>
+
+    <button id="button" onclick="citation()">Citer</button>
+</body>
+</html>
---
+++ job01/script.js
@@ -0,0 +1,7 @@
+function citation() { // Fonction pour afficher une citation
+    // Récupérer le contenu de l'élément ayant l'id "citation"
+    const texte = document.getElementById("citation").textContent; // Récupération du texte
+    
+    // Afficher le contenu dans la console
+    console.log(texte);
+}
