be768fc4774555d69ce7d35227869fa07dc7ed52
Dylan-Dufour <dylan.dufour@laplateforme.io>
2025-11-04 10:37:10 +0100

    Créez une balise <button> et sélectionnez-la dans votre code JavaScript.

--- job01/script.js
+++ job01/script.js
@@ -1,7 +1,13 @@
-function citation() { // Fonction pour afficher une citation
-    // Récupérer le contenu de l'élément ayant l'id "citation"
-    const texte = document.getElementById("citation").textContent; // Récupération du texte
-    
-    // Afficher le contenu dans la console
-    console.log(texte);
-}
+const button = document.getElementById("btn");
+const buttonMasque = document.getElementById("none");
+const textElement = document.getElementById("text");
+button.addEventListener("click", function() {
+    textElement.style.display = "block";
+    textElement.textContent = "Les logiciels et les cathédrales, c'est un peu la même chose - d'abord on les construit,ensuite on prie.";   
+
+});
+buttonMasque.addEventListener("click", function() {    
+    textElement.style.display = "none";
+
+});
+
